746e4f66d13fb7da1910566c1101279cb1cb1aa4
Андрей <107792656+streetraceing@users.noreply.github.com>
2025-03-03 18:08:43 +0300

    Delete src/main/java/net/strdev/telegramlib/json/base directory

--- src/main/java/net/strdev/telegramlib/json/base/AffiliateInfo.java
+++
@@ -1,9 +0,0 @@
-package net.strdev.telegramlib.json.types.base;
-
-public class AffiliateInfo {
-    public User affiliate_user;
-    public Chat affiliate_chat;
-    public Integer commission_per_mille;
-    public Integer amount;
-    public Integer nanostar_amount;
-}
