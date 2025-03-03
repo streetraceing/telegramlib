15ad7cf6d0358d6691b7830e8d0903e877c01b58
Андрей <107792656+streetraceing@users.noreply.github.com>
2025-03-03 18:08:59 +0300

    Add files via upload

---
+++ src/main/java/net/strdev/telegramlib/json/types/base/AffiliateInfo.java
@@ -0,0 +1,9 @@
+package net.strdev.telegramlib.json.types.base;
+
+public class AffiliateInfo {
+    public User affiliate_user;
+    public Chat affiliate_chat;
+    public Integer commission_per_mille;
+    public Integer amount;
+    public Integer nanostar_amount;
+}
