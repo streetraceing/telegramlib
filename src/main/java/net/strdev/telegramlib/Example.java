3a2b6a887b4271741508d3085667bcd7534c51bb
streetraceing <voltzxt@vk.com>
2025-03-04 17:49:58 +0300

    Fixes

--- src/main/java/net/strdev/telegramlib/Example.java
+++ src/main/java/net/strdev/telegramlib/Example.java
@@ -1,20 +1,10 @@
 package net.strdev.telegramlib;
 
 import net.strdev.telegramlib.api.Bot;
-import net.strdev.telegramlib.commands.CommandHandler;
-import net.strdev.telegramlib.requests.sendMessage;
-import net.strdev.telegramlib.types.serializable.ReplyKeyboardRemove;
 
 public class Example {
     public static void main(String[] args) {
         String token = args[0];
         Bot bot = new Bot(token);
-
-        bot.onCommand(new CommandHandler("remove_keyboard", response -> {
-            bot.sendRequest(
-                    new sendMessage().chat_id(response.chat.id).text("Удаляем клавиатуру...")
-                    .reply_markup(new ReplyKeyboardRemove())
-            );
-        }));
     }
 }
